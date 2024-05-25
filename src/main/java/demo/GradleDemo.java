58b73bd464bfb70471315b4f30ff8280df039a8e
H K <ksharshakumaranekal@gmail.com>
2024-05-25 15:07:34 +0530

    Update GradleDemo.java

--- src/main/java/demo/GradleDemo.java
+++ src/main/java/demo/GradleDemo.java
@@ -7,5 +7,7 @@ public class GradleDemo {
 
         System.out.println("Today is demo class");
         System.out.println("Today is demo class but we are executing the commands");
+        String textInBold = "Java_Prof_Level";
+        System.out.print("\033[0;1m" + textInBold);
     }
 }
