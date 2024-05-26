e6fd509a3214ad591cdafffd0e33c8303c7586ad
H K <ksharshakumaranekal@gmail.com>
2024-05-26 18:08:00 +0530

    Update GradleDemo.java

--- src/main/java/demo/GradleDemo.java
+++ src/main/java/demo/GradleDemo.java
@@ -10,6 +10,19 @@ public class GradleDemo {
         String textInBold = "Java_Prof_Level";
         System.out.print("This is for checking polling of job");
         System.out.printf("Job is been completed");
+
+        String str= "Harsha", nstr="";
+        char ch;
+
+        System.out.print("Original word: ");
+        System.out.println("Harsha"); //Example word
+
+        for (int i=0; i<str.length(); i++)
+        {
+            ch= str.charAt(i); //extracts each character
+            nstr= ch+nstr; //adds each character in front of the existing string
+        }
+        System.out.println("Reversed word: "+ nstr);
     }
     
 }
